6969d88f2e9e7e94d694c9ef0b876b06ed5f5698
Ben-Bingham <ben@benbingham.ca>
2025-11-11 14:44:51 -0700

    Switched how bottom bar is passed

--- pLUCK/app/src/main/java/com/pluck/ui/components/AutoHidingBarScroller.kt
+++ pLUCK/app/src/main/java/com/pluck/ui/components/AutoHidingBarScroller.kt
@@ -2,25 +2,36 @@ package com.pluck.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.BottomAppBarScrollBehavior
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.pluck.ui.theme.PluckTheme
+import kotlin.math.roundToInt
 
 /**
  * A generic full screen composable that shows the user a list of items (listElements), however
@@ -68,10 +79,12 @@ fun AutoHidingBarScroller(
         bottomBar = {
             BottomAppBar(
                 modifier = Modifier.fillMaxWidth(),
-                scrollBehavior = bottomBarScrollBehavior
-            ) {
-                bottomBar()
-            }
+                scrollBehavior = bottomBarScrollBehavior,
+                containerColor = PluckPalette.Surface,
+                content = {
+                    bottomBar
+                },
+            )
         }
     )
     { paddingValues  ->
