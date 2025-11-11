fb2a494e44a80c87940ebd648439e74158460de9
Ben-Bingham <ben@benbingham.ca>
2025-11-10 20:47:37 -0700

    Added time picker

--- pLUCK/app/src/main/java/com/pluck/ui/components/DateTimePickers.kt
+++ pLUCK/app/src/main/java/com/pluck/ui/components/DateTimePickers.kt
@@ -1,6 +1,9 @@
 package com.pluck.ui.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -14,10 +17,13 @@ import androidx.compose.material3.DatePickerFormatter
 import androidx.compose.material3.DateRangePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberDateRangePickerState
+import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,9 +33,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.times
+import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import java.time.Instant
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
@@ -168,5 +176,54 @@ fun PLuckDateRangePicker(
                 .fillMaxWidth()
                 .height(LocalConfiguration.current.screenHeightDp.dp * 0.8f)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PLuckTimePicker(
+    onTimeSelected: (LocalTime) -> Unit,
+    onDismiss: () -> Unit,
+    defaultTime: LocalTime
+) {
+    val timePickerState = rememberTimePickerState()
+
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface(
+            shape = RoundedCornerShape(36.dp),
+            color = PluckPalette.Surface,
+        ) {
+            Column {
+                TimePicker(
+                    modifier = Modifier.padding(24.dp),
+                    state = timePickerState
+                )
+
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    TextButton(
+                        onClick = {
+                            onDismiss()
+                        }
+                    ) {
+                        Text("Cancel")
+                    }
+
+                    TextButton(
+                        onClick = {
+                            onTimeSelected(LocalTime.of(timePickerState.hour, timePickerState.minute))
+                            onDismiss()
+                        }
+                    ) {
+                        Text("Ok")
+                    }
+                }
+            }
+        }
     }
 }
