094a2f9b305eed8baba154e9c9192a37bf40ba71
Ben-Bingham <ben@benbingham.ca>
2025-11-12 11:06:20 -0700

    Heavily simplified autoHidingScroller

--- pLUCK/app/src/main/java/com/pluck/ui/components/AutoHidingBarScroller.kt
+++ pLUCK/app/src/main/java/com/pluck/ui/components/AutoHidingBarScroller.kt
@@ -1,17 +1,23 @@
 package com.pluck.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.BottomAppBarScrollBehavior
@@ -19,16 +25,24 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.contentColorFor
+import androidx.compose.material3.rememberBottomAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.pluck.ui.theme.PluckTheme
 import kotlin.math.roundToInt
@@ -52,33 +66,26 @@ import kotlin.math.roundToInt
  *      be as small as possible.
  * @param additionalContent Additional content to be added after scrollable elements
  */
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun AutoHidingBarScroller(
     listElements: List<ComposableItem>,
     indexOfPersistentElement: Int,
     bottomBar: @Composable () -> Unit,
     spacingBetweenItems: Dp = 16.dp,
-    numberOfScrollableElementsForOneScrollable: Int = 2,
     additionalContent: @Composable () -> Unit = { },
 ) {
     // Must be a valid index
     assert(indexOfPersistentElement < listElements.size)
 
-    val scrollableElements = listElements.size - (indexOfPersistentElement + 1)
-
     val listState = rememberLazyListState()
+    val bottomBarState = rememberBottomAppBarState()
 
-    var bottomBarScrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior()
+    var bottomBarScrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior(
+        state = bottomBarState
+    )
     if (!listState.canScrollBackward) {
         bottomBarScrollBehavior = BottomAppBarDefaults.exitAlwaysScrollBehavior(canScroll = { false })
-    }
-
-    val overviewCollapsed by remember {
-        derivedStateOf {
-            listState.firstVisibleItemIndex > 0 ||
-                    listState.firstVisibleItemScrollOffset > 0
-        }
     }
 
     Scaffold(
@@ -95,80 +102,65 @@ fun AutoHidingBarScroller(
         }
     )
     { paddingValues  ->
+
         PluckLayeredBackground(modifier = Modifier.fillMaxSize()) {
-            if (scrollableElements < numberOfScrollableElementsForOneScrollable) {
-                LazyColumn(
-                    state = listState,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(
-                            bottom = paddingValues.calculateBottomPadding(),
-                            start = 16.dp,
-                            end = 16.dp
-                        ),
-                )
-                {
-                    item {
-                        Spacer(modifier = Modifier.height(spacingBetweenItems))
-                    }
+            LazyColumn(
+                state = listState,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        bottom = paddingValues.calculateBottomPadding(),
+                    ),
+            )
+            {
+                item {
+                    Spacer(modifier = Modifier.height(spacingBetweenItems))
+                }
 
-                    listElements.forEach { listElement ->
+                for (i in listElements.indices) {
+                    if (i != indexOfPersistentElement) {
                         item {
-                            listElement.content()
+                            Box(
+                                modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+                            ) {
+                                listElements[i].content()
+                            }
+                        }
+
+                        if (i != indexOfPersistentElement - 1) {
+                            item {
+                                Spacer(modifier = Modifier.height(spacingBetweenItems))
+                            }
+                        }
+
+                    } else {
+                        item {
+                            Spacer(modifier = Modifier.height(spacingBetweenItems / 2))
+                        }
+
+                        stickyHeader {
+                            Surface(
+                                color = PluckPalette.Secondary,
+                                shape = RoundedCornerShape(36.dp),
+                                modifier = Modifier.padding(spacingBetweenItems / 2)
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .padding(spacingBetweenItems / 2)
+                                ) {
+                                    listElements[indexOfPersistentElement].content()
+                                }
+                            }
                         }
 
                         item {
-                            Spacer(modifier = Modifier.height(spacingBetweenItems))
+                            Spacer(modifier = Modifier.height(spacingBetweenItems / 2))
                         }
                     }
                 }
             }
-            else {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(
-                            top = spacingBetweenItems,
-                            bottom = paddingValues.calculateBottomPadding(),
-                            start = 16.dp,
-                            end = 16.dp
-                        ),
-                )
-                {
-                    AnimatedVisibility(visible = !overviewCollapsed) {
-                        Column {
-                            for (i in listElements.indices) {
-                                if (i < indexOfPersistentElement) {
-                                    listElements[i].content()
-
-                                    Spacer(modifier = Modifier.size(spacingBetweenItems))
-                                }
-                            }
-                        }
-                    }
-
-                    listElements[indexOfPersistentElement].content()
-
-                    Spacer(modifier = Modifier.size(spacingBetweenItems))
-
-                    LazyColumn(state = listState)
-                    {
-                        for (i in listElements.indices) {
-                            if (i > indexOfPersistentElement) {
-                                item {
-                                    listElements[i].content()
-                                }
-
-                                item {
-                                    Spacer(modifier = Modifier.height(spacingBetweenItems))
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            additionalContent()
         }
+
+        additionalContent()
     }
 }
